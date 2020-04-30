116b762835f2d7ffff4b68f56b33903831fb2141
Elena Tokmakova <elena.tokmakova@valtech.com>
2020-04-30 00:13:37 -0400

    Added Sweet alert for popup notifications

--- client/components/SearchResults.jsx
+++ client/components/SearchResults.jsx
@@ -1,45 +1,57 @@
-import React, {Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { MDBRow, MDBCol, MDBBtn } from "mdbreact";
 import ShoppingList from './ShoppingList';
 import SweetAlert from 'sweetalert2-react';
 import grocery_store_isle from "../assets/images/grocery_store_isle.png";
 
-const SearchResults = props => {
-    const { selectedPlace, productLocation, groceryList, removeItemFromShoppingList, clearShoppingList, navigateToStore } = props;
-    console.log("Props", props)
-    return (
-        <Fragment>
-            <div><p className="font-weight-bold">{productLocation}</p></div>
-            <MDBRow className="product-search-results">
-                <MDBCol md="12" lg="6">
-                    <img className="image-grocery-isle img-fluid" src={grocery_store_isle} />
-                </MDBCol>
-                <MDBCol md="12" lg="6">
-                    {
-                    props.groceryList.length > 0 && <ShoppingList
-                        groceryList={groceryList}
-                        removeItemFromShoppingList={removeItemFromShoppingList}
-                        clearShoppingList={clearShoppingList}
-                        />
-                    }
-                </MDBCol>
-            </MDBRow>
-            <MDBRow className="product-search-results--action-buttons">
-                <MDBBtn className="back-to-search-button btn-orange-red">
-                    Save this Map
-                </MDBBtn>
-                <Link to={{
-                    pathname: '/select-products',
-                    state: {selectedPlace}
-                }} onClick={navigateToStore}>
-                    <MDBBtn className="back-to-search-button btn-lighter-green">
-                        Ask another question
+class SearchResults extends Component {
+    state = {
+        show: false
+    }
+    render () {
+        const { selectedPlace, productLocation, groceryList, removeItemFromShoppingList, clearShoppingList, navigateToStore } = this.props;
+        return (
+            <Fragment>
+                <div><p className="font-weight-bold">{productLocation}</p></div>
+                <MDBRow className="product-search-results">
+                    <MDBCol md="12" lg="6">
+                        <img className="image-grocery-isle img-fluid" src={grocery_store_isle} />
+                    </MDBCol>
+                    <MDBCol md="12" lg="6">
+                        {
+                        this.props.groceryList.length > 0 && <ShoppingList
+                            groceryList={groceryList}
+                            removeItemFromShoppingList={removeItemFromShoppingList}
+                            clearShoppingList={clearShoppingList}
+                            />
+                        }
+                    </MDBCol>
+                </MDBRow>
+                <MDBRow className="product-search-results--action-buttons">
+                    <MDBBtn className="back-to-search-button btn-orange-red" onClick={ () => this.setState({ show:true })}>
+                        Save this Map
                     </MDBBtn>
-                </Link>
-            </MDBRow>
-        </Fragment>
-    )
+                    <SweetAlert
+                        type="success"
+                        confirmButtonColor="#a1bf63"
+                        show={this.state.show}
+                        title="Success"
+                        text="Your map has been saved!"
+                        onConfirm={() => this.setState({ show: false })}
+                    />
+                    <Link to={{
+                        pathname: '/select-products',
+                        state: {selectedPlace}
+                    }} onClick={navigateToStore}>
+                        <MDBBtn className="back-to-search-button btn-lighter-green">
+                            Ask another question
+                        </MDBBtn>
+                    </Link>
+                </MDBRow>
+            </Fragment>
+        )
+    }
 }
 
 export default withRouter(SearchResults);
--- client/components/ShoppingList.jsx
+++ client/components/ShoppingList.jsx
@@ -1,45 +1,57 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import { MDBTable, MDBTableHead, MDBTableBody, MDBBtn } from "mdbreact";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SweetAlert from 'sweetalert2-react';
 
-const ShoppingList = props => {
-
-    return (
-        <Fragment>
-            <p className="product-search-subtitle">Your shopping list</p>
-            <MDBTable className="shopping-list-table" striped bordered responsive hover>
-                <MDBTableHead>
-                <tr>
-                    <th>Product</th>
-                    <th>Location</th>
-                    <th>Remove</th>
-                </tr>
-                </MDBTableHead>
-                <MDBTableBody>
+class ShoppingList extends Component {
+    state = {
+        show: false
+    }
+    render () {
+        return (
+            <Fragment>
+                <p className="product-search-subtitle">Your shopping list</p>
+                <MDBTable className="shopping-list-table" striped bordered responsive hover>
+                    <MDBTableHead>
+                    <tr>
+                        <th>Product</th>
+                        <th>Location</th>
+                        <th>Remove</th>
+                    </tr>
+                    </MDBTableHead>
+                    <MDBTableBody>
+                    {
+                        this.props.groceryList.map( (product, index) => {
+                            const { name, location } = product;
+                            return (
+                                <tr key={index}>
+                                <td>{name}</td>
+                                <td>{location}</td>
+                                <td><FontAwesomeIcon onClick={this.props.removeItemFromShoppingList.bind(this, name)} icon="trash" className="clear-shopping-list-icon"/></td>
+                                </tr>
+                            )
+                        })
+                    }
+                </MDBTableBody>
+                </MDBTable>
                 {
-                    props.groceryList.map( (product, index) => {
-                        const { name, location } = product;
-                        return (
-                            <tr key={index}>
-                            <td>{name}</td>
-                            <td>{location}</td>
-                            <td><FontAwesomeIcon onClick={props.removeItemFromShoppingList.bind(this, name)} icon="trash" className="clear-shopping-list-icon"/></td>
-                            </tr>
-                        )
-                    })
+                    this.props.groceryList.length > 0 &&
+                    <Fragment>
+                        <MDBBtn className="back-to-product-search-button btn-orange-red" onClick={ () => this.setState({ show:true })}>Save shopping list</MDBBtn>
+                        <SweetAlert
+                            type="success"
+                            confirmButtonColor="#a1bf63"
+                            show={this.state.show}
+                            title="Success"
+                            text="Your shopping list has been saved!"
+                            onConfirm={() => this.setState({ show: false })}
+                        />
+                        <MDBBtn className="back-to-product-search-button btn-lighter-green" onClick={this.props.clearShoppingList}>Clear shopping list</MDBBtn>
+                    </Fragment>
                 }
-            </MDBTableBody>
-            </MDBTable>
-            {
-                props.groceryList.length > 0 &&
-                <Fragment>
-                    <MDBBtn className="back-to-product-search-button btn-orange-red">Save shopping list</MDBBtn>
-                    <MDBBtn className="back-to-product-search-button btn-lighter-green" onClick={props.clearShoppingList}>Clear shopping list</MDBBtn>
-                </Fragment>
-            }
-        </Fragment>
-      );
+            </Fragment>
+          );
+    }
 }
 
 export default ShoppingList;
