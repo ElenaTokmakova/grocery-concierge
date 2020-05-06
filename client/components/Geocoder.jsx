9348d91f4e40c1fdd71675d26644c92f4371fdd4
Elena Tokmakova <elena.tokmakova@valtech.com>
2020-05-06 12:27:17 -0400

    Updated Geocoder component

--- client/components/Geocoder.jsx
+++ client/components/Geocoder.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import Geocode from "react-geocode";
 import { MDBInput, MDBBtn } from "mdbreact";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,62 +6,55 @@ import SweetAlert from 'sweetalert2-react';
 
 Geocode.setApiKey(process.env.API_KEY);
 
-export default class GeocoderInput extends Component {
-    state = {
-        postalCode: '',
-        lat: 43.643500,
-        lng: -79.393520,
-        show: false
-    }
+const GeocoderInput = (props) => {
 
-    getCoordsFromPostalCode = () => {
+    const [show, setShow] = useState(false);
+    const [postalCode, setPostalCode] = useState('');
 
-        if (!this.state.postalCode) {
-            this.setState({ show: true });
+    const getCoordsFromPostalCode = () => {
+
+        if (!postalCode) {
+            setShow(true);
             return;
         }
 
-        Geocode.fromAddress(this.state.postalCode).then(
-            response => {
-              console.log("Geocoder response", response)
+        Geocode.fromAddress(postalCode).then(response => {
               const { lat, lng } = response.results[0].geometry.location;
-              this.setState({
-                  postalCode: ''
-              })
-              this.props.updateCoords(lat, lng)
+              setPostalCode('');
+              props.updateCoords(lat, lng);
             },
             error => {
               console.error(error);
-              this.setState({ show: true });
+              setShow(true);
             }
           );
     }
 
-    onKeyPressed(event) {
+    const onKeyPressed = (event) => {
         if (event.key === "Enter") {
-          this.getCoordsFromPostalCode()
+          getCoordsFromPostalCode();
         }
     }
 
-    render() {
-        return (
-            <div className="form-group geocoder-input-container">
-                <MDBInput label="Enter postal code" outline size="lg" className="goecoder-input"
-                            value={this.state.postalCode}
-                            onKeyPress={(event) => this.onKeyPressed(event)}
-                            onChange={(event) => this.setState({ postalCode: event.target.value })}/>
-                <MDBBtn className="btn-lighter-green geocoder-submit-button" onClick={this.getCoordsFromPostalCode} >
-                    <FontAwesomeIcon icon="angle-right"/>
-                </MDBBtn>
-                <SweetAlert
-                    type="error"
-                    confirmButtonColor="#a1bf63"
-                    show={this.state.show}
-                    title="Error"
-                    text="Please enter a valid postal code"
-                    onConfirm={() => this.setState({ show: false })}
-                />
-            </div>
-        )
-    }
+    return (
+        <div className="form-group geocoder-input-container">
+            <MDBInput label="Enter postal code" outline size="lg" className="goecoder-input"
+                        value={postalCode}
+                        onKeyPress={(event) => onKeyPressed(event)}
+                        onChange={(event) => setPostalCode(event.target.value)}/>
+            <MDBBtn className="btn-lighter-green geocoder-submit-button" onClick={getCoordsFromPostalCode} >
+                <FontAwesomeIcon icon="angle-right"/>
+            </MDBBtn>
+            <SweetAlert
+                type="error"
+                confirmButtonColor="#a1bf63"
+                show={show}
+                title="Error"
+                text="Please enter a valid postal code"
+                onConfirm={() => setShow(false)}
+            />
+        </div>
+    )
 }
+
+export default GeocoderInput;
