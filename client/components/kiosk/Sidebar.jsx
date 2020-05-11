77d8316af5760bd1acf0dab3c5fae52d5de73835
Elena Tokmakova <elena.tokmakova@valtech.com>
2020-05-11 10:21:10 -0400

    Styling kiosk sidebar

--- client/components/kiosk/Sidebar.jsx
+++ client/components/kiosk/Sidebar.jsx
@@ -9,6 +9,7 @@ const Sidebar = () => {
                 <p>361 Front Street W, Toronto, ON</p>
                 <p>M5V 3R5</p>
             </section>
+            <hr />
             <section>
                 <span>Last updated: 1 hour ago</span>
                 <h3 className="underlined"><u>Items low in stock</u></h3>
@@ -19,6 +20,7 @@ const Sidebar = () => {
                     <li>Noodles</li>
                 </ul>
             </section>
+            <hr/>
             <section>
                 <h3 className="underlined"><u>News Bulletin</u></h3>
                 <ul className="list-inline">
